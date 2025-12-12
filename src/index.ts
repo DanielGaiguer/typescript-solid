130235880511c684b0a5449b6629e2cb9061d77d
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-12 14:46:24 -0300

    readme

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/*
+Vantagens e desvantagens do S.O.L.I.D
+
+Vantagens:
+- Código Modular
+- Código reutilizável (D.R.Y - Don't repeat yoursealf)
+- Código testável, baixo acoplamento
+- Baixo Acoplamento e alta coerência
+- Código de facíl expansão
+- Separations of concerns ( Separação de Conceitos )
+- Facíl Manutenção
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta bastante
+- Tenha cuidados com: YAGNI, KISS (You aren't gonna need it, Keep itsimple, stupid)
+
+*/
