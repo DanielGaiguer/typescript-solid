d366d24a6eeb41b1395db3b5dbb192787ef27d2d
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-12 08:28:42 -0300

    Create base ISP

---
+++ src/isp/main.ts
@@ -0,0 +1,30 @@
+/*
+Interface segregation Principle (Principio da segregação de Interface) -
+os clientes não devem ser forçados a depender de interfaces que não utilizam
+*/
+
+import { Messaging } from './services/messaging';
+import { Order } from './Class/order';
+import { Persistency } from './services/persistency';
+import { Product } from './Class/products';
+import { ShoppingCart } from './Class/shopping-cart';
+import { TenPercentDiscount } from './Class/discount';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const noDiscount = new NoDiscount();
+const tenPercentDiscount = new TenPercentDiscount();
+const shoppingCart = new ShoppingCart(tenPercentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('camiseta', 49.9));
+shoppingCart.addItem(new Product('caderno', 9.9));
+shoppingCart.addItem(new Product('lapis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWidthDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
